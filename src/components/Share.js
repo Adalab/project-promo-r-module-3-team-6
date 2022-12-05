ce29d9632ae1e0d7ff8d8fe514d51ea4d62bd0f0
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-12-05 10:49:08 +0100

    Add target blank to url of card

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,5 +1,4 @@
 function Share(props) {
-
   const handleShare = (ev) => {
     ev.preventDefault();
     props.handleShare();
@@ -22,8 +21,16 @@ function Share(props) {
       </fieldset>
       <section className='form__collapsible js_share_section'>
         <div className='form__collapsible__boxText js_box_share_card'></div>
-        <a href={props.dataResult.success && props.dataResult.cardURL}>   {props.dataResult.success ? props.dataResult.cardURL : props.dataResult.error}</a>
-        <a href={props.dataResult.success && `https://twitter.com/intent/tweet?text=Mira%20mi%20super%20tarjeta%20de%20visita%20creada%20con%20StarCards&url=${props.dataResult.cardURL}`} className='form__collapsible__shareBtn js_twitter_btn'>
+        <a href={props.dataResult.success && props.dataResult.cardURL} target='_blank' rel='noopener noreferrer'>
+          {props.dataResult.success ? props.dataResult.cardURL : props.dataResult.error}
+        </a>
+        <a
+          href={
+            props.dataResult.success &&
+            `https://twitter.com/intent/tweet?text=Mira%20mi%20super%20tarjeta%20de%20visita%20creada%20con%20StarCards&url=${props.dataResult.cardURL}`
+          }
+          className='form__collapsible__shareBtn js_twitter_btn'
+        >
           <i className='fa-brands fa-twitter'></i>
           <span>Compartir en twitter</span>
         </a>
