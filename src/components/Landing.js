068e1a4def7ca7c1dcfdda5f2b3e0e4116e0b68e
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-12-11 11:03:07 +0100

    Edit size of icons on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -17,15 +17,15 @@ const Landing = () => {
         <p className='landing__text'>Crea contactos profesionales estelares de forma fácil y cómoda</p>
         <div className='landing__icons'>
           <div className='landing__icons__item'>
-            <i className='fa regular fa-object-ungroup ungroup_landing'></i>
+            <i className='fa regular fa-object-ungroup landing_icon'></i>
             <p className='landing__icons__item__text'>Diseña</p>
           </div>
           <div className='landing__icons__item icon_fill'>
-            <i className='fa regular fa-keyboard keyboard_landing'></i>
+            <i className='fa regular fa-keyboard landing_icon'></i>
             <p className='landing__icons__item__text'>Rellena</p>
           </div>
           <div className='landing__icons__item'>
-            <i className='fa regular fa-share-nodes nodes_landing'></i>
+            <i className='fa regular fa-share-nodes landing_icon'></i>
             <p className='landing__icons__item__text'>Comparte</p>
           </div>
         </div>
